b1881a0176ea0c5f96e2c621893d1a7b42e81e94
Elysia <23010777@st.phenikaa-uni.edu.vn>
2025-09-05 07:20:12 +0000

    update all changes

---
+++ Music-App/src/Controller/Leaf.java
@@ -0,0 +1,14 @@
+package Controller;
+
+public class Leaf {
+    int i = 0;
+    Leaf increment() {
+        i++;
+        return this;//Leaf
+    }
+    void print() {System.out.println("i="+i);}
+    public static void main(String[] args) {
+        Leaf x = new Leaf();
+        x.increment().increment().increment().print();
+    }
+}
