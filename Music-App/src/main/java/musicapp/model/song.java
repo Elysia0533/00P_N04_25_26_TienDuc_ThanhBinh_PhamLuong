c13a9ed7cdefd8ffcf0b08a124a1670934bac50c
Elysia <23010777@st.phenikaa-uni.edu.vn>
2025-09-05 06:56:42 +0000

    update all changes

--- Music-App/src/main/java/musicapp/model/song.java
+++ Music-App/src/main/java/musicapp/model/song.java
@@ -1,5 +1,20 @@
 package main.java.musicapp.model;
 
 public class song {
-    
+    String title;
+    String artistName;
+    int durationInSecond;
+    String filePath;
+
+    public Song() {
+        this.title = "Unknown Title";
+        this.artistName = "Unknown Artist";
+    }
+
+    public Song(String title, String artistName, int durationInSecond, String filePath) {
+        this.title = title;
+        this.artistName = artistName;
+        this.this.durationInSecond = durationInSecond;
+        this.filePath = filePath;
+    }
 }
