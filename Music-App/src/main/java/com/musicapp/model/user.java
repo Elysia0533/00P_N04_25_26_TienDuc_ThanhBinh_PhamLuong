2f830a6ff4a78585e7d512521a8b3d1e8d317c22
Elysia <23010777@st.phenikaa-uni.edu.vn>
2025-09-08 09:37:48 +0000

    update all changes

--- Music-App/src/main/java/com/musicapp/model/user.java
+++ Music-App/src/main/java/com/musicapp/model/user.java
@@ -1,5 +1,35 @@
 package main.java.musicapp.model;
 
 public class user {
-    
+    private String id;
+    private String username;
+    private String email;
+
+    public User(String id, String username, String email) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+    }
+    public String getId() {
+        return id;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        if (user name == null || username.isEmpty()) {
+            this.username = "Unknown";
+        } else {this.username = username;}
+    }
+    public String getEmail() {returnemail;}
+    public void setEmail(String email) {
+        if (email != null && email.contain("@")) {
+            this.email = email;
+        } else {
+            this.email = "invalid";
+        }
+    }
+    public String toString() {
+        return "User{id='" + getId() + "', username='" + getUsername() + "', email='" + getEmail() + "'}";
+    }
 }
