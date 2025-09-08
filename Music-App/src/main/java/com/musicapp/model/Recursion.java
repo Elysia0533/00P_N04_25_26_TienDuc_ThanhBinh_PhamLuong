dba8d88b1e270d875bd9a2dc36125b9b08bab448
binh17-2 <23010242@st.phenikaa-uni.edu.vn>
2025-09-08 09:37:23 +0000

    update all changes

---
+++ Music-App/src/main/java/com/musicapp/model/Recursion.java
@@ -0,0 +1,24 @@
+package src;
+
+public class Recursion {
+
+
+    public static long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+        return n * factorial(n - 1);
+    }
+
+    public static long fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("5! = " + factorial(5));
+        System.out.println("Fibonacci(10) = " + fibonacci(10));
+    }
+}
