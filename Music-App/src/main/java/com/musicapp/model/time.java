948a673428ccf179e9cbababa0158e90130e352f
binh17-2 <23010242@st.phenikaa-uni.edu.vn>
2025-09-08 09:31:04 +0000

    update all changes

---
+++ Music-App/src/main/java/com/musicapp/model/time.java
@@ -0,0 +1,45 @@
+
+ package src;
+
+import java.time.LocalDateTime;
+import java.time.Duration;
+import java.time.format.DateTimeFormatter;
+
+public class Time {
+    private LocalDateTime dateTime;
+
+    public Time() {
+        this.dateTime = LocalDateTime.now(); // mặc định lấy thời gian hiện tại
+    }
+
+    public Time(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    
+    public LocalDateTime getDateTime() {
+        return dateTime;
+    }
+
+    
+    public void setDateTime(LocalDateTime dateTime) {
+        this.dateTime = dateTime;
+    }
+
+   
+    public String format(String pattern) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(pattern);
+        return dateTime.format(formatter);
+    }
+
+    
+    public void addMinutes(long minutes) {
+        this.dateTime = this.dateTime.plusMinutes(minutes);
+    }
+
+    
+    public static long differenceInMinutes(Time t1, Time t2) {
+        Duration diff = Duration.between(t1.getDateTime(), t2.getDateTime());
+        return diff.toMinutes();
+    }
+}
