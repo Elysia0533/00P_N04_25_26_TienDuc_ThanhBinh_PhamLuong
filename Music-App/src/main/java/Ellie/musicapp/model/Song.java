6e8c4c1ee97cba74af23c62f05ee395fb83b7c8b
binh17-2 <23010242@st.phenikaa-uni.edu.vn>
2025-09-19 08:24:10 +0000

    update all changes

--- Music-App/src/main/java/Ellie/musicapp/model/Song.java
+++ Music-App/src/main/java/Ellie/musicapp/model/Song.java
@@ -1,22 +1,4 @@
-/*  public class Song {
-    String title;
-    String artistName;
-    int durationInSecond;
-    String filePath;
 
-    public Song() {
-        this.title = "Unknown Title";
-        this.artistName = "Unknown Artist";
-    }
-
-    public Song(String title, String artistName, int durationInSecond, String filePath) {
-        this.title = title;
-        this.artistName = artistName;
-        this.durationInSecond = durationInSecond;
-        this.filePath = filePath;
-    }
-}
-*/
 package Ellie.musicapp.model;
 import java.io.Serializable;
 import class Song implements Serializable{
