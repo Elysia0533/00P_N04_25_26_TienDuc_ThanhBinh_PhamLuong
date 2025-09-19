4369c85b9a6cbaea38d4cf3d182cbb91b1aefcd7
binh17-2 <23010242@st.phenikaa-uni.edu.vn>
2025-09-19 07:30:48 +0000

    update all changes

--- Music-App/src/main/java/Ellie/musicapp/model/Song.java
+++ Music-App/src/main/java/Ellie/musicapp/model/Song.java
@@ -17,3 +17,37 @@
     }
 }
 */
+import class Song implements Serializable{
+    private Lon id;
+    private String title;
+    private int durationInSecond;
+    private String filePath;
+
+    public Song(){}
+    public Song(long id, String title, int durationInSecond, String filePath){
+        this.id = id;
+        this.title = title;
+        this.durationInsecond = durationInSecond;
+        this.filePath = filePath;
+    }
+    public Long getId(){return id; }
+    public void setId(Long id){this.id = id; }
+
+    public String getTitle(){return title;}
+    public void setTitle(String title){this.title = title; }
+
+public int getDurationInSecond(){return durationInSecond; }
+public void setDurationInSecond(int durationInSecond){this.durationInSecond = durationInSecond; }
+
+public String getFilePath(){ return filePath; }
+public void setFilePath(String filePath){this.filePath = filePath; }
+
+public String toString(){
+    return "Song{"
+    "id=" + id +
+    ", title='" + title + '\'' +
+    ", durationInSecond=" + durationInSecond +
+    ", filePath='" + filePath + '\''+
+    '}';
+}
+}
