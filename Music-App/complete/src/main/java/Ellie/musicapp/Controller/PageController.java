5764258e0069e7e19f0cc9190e39377168e33c92
Elysia <23010777@st.phenikaa-uni.edu.vn>
2025-10-30 14:52:29 +0000

    html

--- Music-App/complete/src/main/java/Ellie/musicapp/Controller/PageController.java
+++ Music-App/complete/src/main/java/Ellie/musicapp/Controller/PageController.java
@@ -2,10 +2,8 @@ package Ellie.musicapp.Controller;
 
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
 
 @Controller
 public class PageController {
@@ -15,15 +13,43 @@ public class PageController {
         return "index";
     }
 
+    @GetMapping("/songs")
+    public String songs() {
+        return "songs";
+    }
+
+    @GetMapping("/playlists")
+    public String playlists() {
+        return "playlists";
+    }
+
     @GetMapping("/login")
-    public String showLoginPage() {
+    public String login() {
         return "login";
     }
 
-    @PostMapping("/login")
-    public String doLogin(@RequestParam String username, @RequestParam String password, Model model) {
+    @GetMapping("/signup")
+    public String signup() {
+        return "signup";
+    }
 
-        model.addAttribute("user", username);
-        return "redirect:/";
+    @GetMapping("/artist")
+    public String artist(Model model) {
+        model.addAttribute("artist", new Object());
+        model.addAttribute("songs", new Object());
+        return "artist";
+    }
+
+    @GetMapping("/album")
+    public String album(Model model) {
+        model.addAttribute("album", new Object());
+        return "album";
+    }
+
+    @GetMapping("/profile")
+    public String profile(Model model) {
+        model.addAttribute("user", new Object());
+        model.addAttribute("playlists", new Object());
+        return "profile";
     }
 }
